7e258f58c0e8c8d0a366c419fddca7bfe513ff55
Sergio <sergioeiman30@gmail.com>
2020-10-04 19:22:54 +0200

    config update

--- project.py
+++ project.py
@@ -3,6 +3,19 @@ from app.models import User, Post
 
 app = create_app()
 
+
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {
+        "db": db,
+        "User": User,
+        "Post": Post,
+        "CustomerOrderDetails": CustomerOrderDetails,
+        "OrderItem": OrderItem,
+        "Product": Product,
+        "Order": Order,
+        "RetailStores": RetailStores,
+        "Category": Category,
+        "Aisles": Aisles,
+    }
+
