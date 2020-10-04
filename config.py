5c0e1ce03b2248b71acfc465bf5e757f13853baa
Sergio <sergioeiman30@gmail.com>
2020-10-04 19:35:12 +0200

    config update 3

--- config.py
+++ config.py
@@ -1,28 +1,30 @@
-import os 
+import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
+load_dotenv(os.path.join(basedir, ".env"))
+
 
 class Config(object):
-    SECRET_KEY=os.environ.get('SECRET_KEY') or 'secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "secret"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = ["your-email@example.com"]
     POSTS_PER_PAGE = 15
     SHOPAISLES_PER_PAGE = 15
     PRODUCTS_PER_PAGE = 3
     CATEGORY_PER_PAGE = 15
-    UPLOAD_FOLDER = '/app/static/uploads'
-    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-    UPLOADED_PHOTOS_DEST = os.path.join(basedir, 'app/static/images/profileImages')
+    UPLOAD_FOLDER = "/app/static/uploads"
+    ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
+    ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL")
+    UPLOADED_PHOTOS_DEST = os.path.join(basedir, "app/static/images/profileImages")
     MAX_CONTENT_LENGTH = 1024 * 1024
-    UPLOAD_EXTENSIONS = ['.jpg', '.png', '.gif', '.png']
-    UPLOAD_PATH = 'app/static/images/profileImages'
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    UPLOAD_EXTENSIONS = [".jpg", ".png", ".gif", ".png"]
+    UPLOAD_PATH = "app/static/images/profileImages"
+    LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT")
+
