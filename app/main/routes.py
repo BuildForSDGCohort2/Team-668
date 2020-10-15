f8d307ac1180b183619c8eb2772f466cdf8c2f30
Sergio <sergioeiman30@gmail.com>
2020-10-15 14:37:09 +0200

    fb messenger update

--- app/main/routes.py
+++ app/main/routes.py
@@ -669,7 +669,10 @@ def fb_receive_message():
             if message.get("message"):
                 print("{sender[id]} says {message[text]}".format(**message))
                 if "text" in message["message"]:
-                    text_reply = "Hi, How can I help"
+                    if "Hi" in message["message"]["text"]:
+                        text_reply = "Hi, How can I help"
+                    elif "Order" in message["message"]["text"]:
+                        text_reply = "Hi , How can I help with your order"
                 client.send_text(user_id, text_reply)
 
     return "Hi"
